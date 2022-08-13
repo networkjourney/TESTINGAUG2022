32c28fb4a6164fc1a0f22468554cdde121f4dea9
networkjourney <62839860+networkjourney@users.noreply.github.com>
2022-08-13 19:28:23 +0530

    I CREATED NEW PYTHON SCRIPT

---
+++ MYFIRSTPYTHONSCRPT.PY.py
@@ -0,0 +1,11 @@
+vices123 in readfrmfile:
+#     data123 = {
+#         "ip": singledevices123,
+#         "username": user123,
+#         "password": pass123,
+#         "port": 22,
+#         "device_type": "cisco_ios",
+#         "secret": "cisco1234",
+#         "conn_timeout": 10,
+#     }
+#     try:
